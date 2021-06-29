2d07468158a6af772b5c85cf468b3d61305e65e5
unknown <alisson.santiago123@gmail.com>
2021-06-28 23:25:01 -0300

    make it better

--- apps/mensal.py
+++ apps/mensal.py
@@ -197,7 +197,7 @@ def update_graph1(stations_value,clima_value,graph_type, df):
 
         t_2=0
         for j in clima_value:
-            df2 = df_clima[df_clima.index == j].loc[:,8:-2].T
+            df2 = df_clima[df_clima.index == j].loc[:,7:-2].T
             trace_1.append(go.Scatter(name=j, x=df2.index, y=df2[j], line={'dash': 'dash'}, line_color = colors2[t_2]))
             t_2+=1
         data = trace_1
@@ -212,7 +212,7 @@ def update_graph1(stations_value,clima_value,graph_type, df):
 
         t_2=0
         for j in clima_value:
-            df2 = df_clima[df_clima.index == j].iloc[:,8:-2].T
+            df2 = df_clima[df_clima.index == j].iloc[:,7:-2].T
             trace_1.append(go.Scatter(name=j, x=df2.index, y=df2[j], line={'dash': 'dash'}, line_color = colors2[t_2]))
             t_2+=1
         data = trace_1
@@ -327,7 +327,7 @@ def update_graph2(station,years, stations_clima, df):
 
     t_2=0
     for j in stations_clima:
-        df2 = df_clima[df_clima.index == j].iloc[:,8:-2].T
+        df2 = df_clima[df_clima.index == j].iloc[:,7:-2].T
         trace_1.append(go.Scatter(name=j, x=df2.index, y=df2[j], line={'dash': 'dash'}, line_color = colors2[t_2]))
         t_2+=1
     data = trace_1
