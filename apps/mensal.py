5e64dfd94994240f8761d038199526467facc1ee
Alisson Santiago Amaral <67293732+SantiagoAmaral@users.noreply.github.com>
2022-01-05 13:57:37 -0300

    Update mensal.py

--- apps/mensal.py
+++ apps/mensal.py
@@ -223,7 +223,7 @@ def update_graph1(stations_value,clima_value,graph_type, df):
 
         
         j = clima_value
-        df2 = df_clima2[df_clima2.index == j].iloc[:,7:-2].T
+        df2 = df_clima2[df_clima2.index == j].iloc[:,7:-1].T
         trace_1.append(go.Scatter(name=j, x=df2.index, y=df2[j], line={'dash': 'dash'}, line_color = '#080808', text = j))
         
 
@@ -397,7 +397,7 @@ def update_graph2(station,years, stations_clima, df):
 
     t_2=0
     for j in stations_clima:
-        df2 = df_clima2[df_clima2.index == j].iloc[:,7:-2].T
+        df2 = df_clima2[df_clima2.index == j].iloc[:,7:-1].T
         trace_1.append(go.Scatter(name=j, x=df2.index, y=df2[j], line={'dash': 'dash'}, line_color = colors2[t_2]))
         t_2+=1
     data = trace_1
